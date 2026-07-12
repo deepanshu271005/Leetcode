e75af07aec93c0cbcf1d995895cffaa6cb289c62
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-07-13 03:10:39 +0530

    Time: 4 ms (14.77%) | Memory: 22.8 MB (67.34%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -7,38 +7,36 @@ public:
         for(int i=0;i<n;i++){
              int prev=i-1;
              int next=i+1;
-             while(true){
-                if(prev>=0 && rating[i]>rating[prev] && candy[i]<=candy[prev]){
-                    candy[i]++;
-                    ans++;
+            int new_candy=0;
+             if(prev>=0 && rating[i]>rating[prev] && candy[i]<=candy[prev]){
+                      new_candy=max(new_candy,candy[prev]);
+                     
                 }
-            else if(next<n && rating[i]>rating[next] && candy[i]<=candy[next]){
-                    candy[i]++;
-                    ans++;
+             if(next<n && rating[i]>rating[next] && candy[i]<=candy[next]){
+                     new_candy=max(new_candy,candy[next]);
                 }
-                else {
-                    break;
-                }
-             }
+                
+           if(new_candy!=0) {  int more_candies=new_candy-candy[i]+1;
+              ans+=more_candies;
+              candy[i]=new_candy+1;}
         }
 
 
             for(int i=n-1;i>=0;i--){
              int prev=i+1;
              int next=i-1;
-             while(true){
+              int new_candy=0;
                 if(prev<n && rating[i]>rating[prev] && candy[i]<=candy[prev]){
-                    candy[i]++;
-                    ans++;
+                       new_candy=max(new_candy,candy[prev]);
                 }
-            else if(next>=0 && rating[i]>rating[next] && candy[i]<=candy[next]){
-                    candy[i]++;
-                    ans++;
+              if(next>=0 && rating[i]>rating[next] && candy[i]<=candy[next]){
+                       new_candy=max(new_candy,candy[next]);
                 }
-                else {
-                    break;
-                }
-             }
+               
+               if(new_candy!=0) {  int more_candies=new_candy-candy[i]+1;
+              ans+=more_candies;
+              candy[i]=new_candy+1;}
+             
         }
          
         return ans;
