f2fe19931bf793ba7e199dd322da2f6d88637eab
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-07-14 02:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (11.63%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        return sort(nums.begin(),nums.end());
-         
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)ans.push_back(0);
+        }
+         for(int i=0;i<nums.size();i++){
+            if(nums[i]==1)ans.push_back(1);
+        }
+         for(int i=0;i<nums.size();i++){
+            if(nums[i]==2)ans.push_back(2);
+        }
+        nums=ans;
+        return ; 
     }
 };
