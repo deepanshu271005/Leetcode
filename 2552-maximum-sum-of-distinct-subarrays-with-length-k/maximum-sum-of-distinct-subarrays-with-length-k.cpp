61d447b72b25c261748f3380c9cd98326d5082f1
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-07-15 12:58:21 +0530

    Time: 107 ms (40.20%) | Memory: 98.3 MB (35.35%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -4,22 +4,20 @@ public:
         unordered_map<int,int>m;
         long long ans=0;
         int l=0,r=0;
-        vector<long long>prefix;
+         
         long long  curr_sum=0;
-        for(int i=0;i<nums.size();i++){
-            curr_sum+=nums[i];
-            prefix.push_back(curr_sum);
-        }
         while(r<nums.size()){
              
              while(m[nums[r]]!=0 || r-l+1>k){
+                curr_sum-=nums[l];
                 m[nums[l]]--;
                 l++;
             }
             m[nums[r]]++;
+            curr_sum+=nums[r];
             if(r-l+1==k){
-                long long left_sum = (l > 0) ? prefix[l - 1] : 0;
-                ans=max(ans,prefix[r]-left_sum);
+                 
+                ans=max(ans,curr_sum);
             }
             r++;
         }
