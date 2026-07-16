511a0dca3dd7ae93f40db3b1256d286455355dc7
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-07-17 00:38:11 +0530

    Time: 0 ms (100.00%) | Memory: 65 MB (91.09%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,10 +1,31 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //easiest is if i sort this and then compare all the adjacent element 
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1])return nums[i];
+        // easiest is if i sort this and then compare all the adjacent element
+        //  sort(nums.begin(),nums.end());
+        //  for(int i=1;i<nums.size();i++){
+        //      if(nums[i]==nums[i-1])return nums[i];
+        //  }
+        //  return -1;
+
+        // how to do this in O(n) tc and O(1)sc
+        int fast = 0;
+        int slow = 0;
+
+        while (true) {
+
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (fast == slow) {
+                slow = 0;
+
+                while (fast != slow) {
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return fast;
+            }
         }
         return -1;
     }
