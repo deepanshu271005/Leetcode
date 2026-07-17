5a3f473bd427c1074f2f0eda55d8fb390521cb25
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-07-18 04:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (39.59%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right)
+            return head;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev->next;
+        }
+
+        ListNode* curr = prev->next;
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode* future = curr->next;
+            curr->next = future->next;
+            future->next = prev->next;
+            prev->next = future;
+        }
+
+        ListNode* newHead = dummy->next;
+        delete dummy;
+        return newHead;
+    }
+};
