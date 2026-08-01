4b21553cfbadb17a5fc486e4106d1329f3c1496d
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-01 23:32:07 +0530

    Time: 4 ms (7.70%) | Memory: 15.7 MB (7.65%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        //as the value of n is simply 10 we can use bit masking to create all the subset 
-        int n=nums.size();
-        int total=1<<n;
-        vector<vector<int>>ans;
-        //now there are 0 to 2 powN -1 number whcih each represnet all the subset possible 
-        for(int mask=0;mask<total;mask++){
-            vector<int>temp;
-            for(int i=0;i<11;i++){
-                //check is the ith bit is on or not 
-                if(1<<i & mask){
-                 temp.push_back(nums[i]);
-                }
-            }
+    void f(vector<int>& nums, int idx, vector<vector<int>>& ans, vector<int> temp) {
+        
+        if (idx == nums.size()) {
             ans.push_back(temp);
+            return;
         }
+        
+        
+        f(nums, idx + 1, ans, temp);
+        
+         
+        temp.push_back(nums[idx]);
+        f(nums, idx + 1, ans, temp);
+        
+        
+       // temp.pop_back();
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        
+        f(nums, 0, ans, temp);
+        
         return ans;
     }
 };
