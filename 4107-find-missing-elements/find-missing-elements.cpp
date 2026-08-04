26dc01b2c432d518e4a5f4d3831127eda4e7e260
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-04 11:26:00 +0530

    Time: 1 ms (60.57%) | Memory: 32.3 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -4,13 +4,18 @@ public:
         sort(nums.begin(),nums.end());
         int cnt=nums[0];
         vector<int>ans;
-       vector<int>freq(101,0);
-      for(auto i:nums)freq[i]++;
+        int idx=0;
+       //vector<int>freq(101,0);
+    //  for(auto i:nums)freq[i]++;
         while(cnt<=nums[nums.size()-1]){
-            if(freq[cnt]==0){
-                ans.push_back(cnt);
-            }
+           if(nums[idx]!=cnt){
+            ans.push_back(cnt);
             cnt++;
+           }
+           else {
+            cnt++;idx++;
+           }
+            
         }
         return ans;
     }
