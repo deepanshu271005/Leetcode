b327b3033984badb88017249b9e168a2c544eadf
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-08 03:31:41 +0530

    Time: 1 ms (60.06%) | Memory: 9.1 MB (19.92%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+ void backtrack(vector<string>& board, int row, int n, 
+                   vector<bool>& cols, vector<bool>& antiDiag, vector<bool>& mainDiag, 
+                   vector<vector<string>>& ans) {
+         if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+         for (int col = 0; col < n; col++) {
+            int antiId = row + col;
+            int mainId = row - col + n - 1;
+
+             if (cols[col] || antiDiag[antiId] || mainDiag[mainId]) {
+                continue; 
+            }
+
+             board[row][col] = 'Q';
+            cols[col] = true;
+            antiDiag[antiId] = true;
+            mainDiag[mainId] = true;
+
+             backtrack(board, row + 1, n, cols, antiDiag, mainDiag, ans);
+
+             board[row][col] = '.';
+            cols[col] = false;
+            antiDiag[antiId] = false;
+            mainDiag[mainId] = false;
+        }
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        
+         vector<bool> cols(n, false);
+        vector<bool> antiDiag(2 * n - 1, false);
+        vector<bool> mainDiag(2 * n - 1, false);
+
+         backtrack(board, 0, n, cols, antiDiag, mainDiag, ans);
+
+        return ans.size();
+    }
+};
