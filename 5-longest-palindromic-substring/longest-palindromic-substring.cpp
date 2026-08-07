97464073f0750de736d2932e296ce77757961cf3
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-08 03:15:51 +0530

    Time: 159 ms (22.11%) | Memory: 26.2 MB (23.21%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
- 
-    bool check(string & temp){
-        int l=0,h=temp.size()-1;
-        while(l<=h){
-            if(temp[l]!=temp[h])return false;
-            l++;h--;
-        }
-        return true;
-    }
-
     string longestPalindrome(string s) {
-          int n=s.size();
-          string ans;
-         for(int i=0;i<n;i++){
-             string temp;
-            for(int j=i;j<n;j++){
-                   temp.push_back(s[j]);
-            if(check(temp) && temp.size()>ans.size()){ans=temp;}
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        // for(int i=0;i<n;i++)dp[i][i]=1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i > j)
+                    continue;
+                if (i == j)
+                    dp[i][j] = 1;
+                if (s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1]))
+                    dp[i][j] = true;
             }
-         }
-          return ans;
+        }
+        int ans = 0;
+        int x = 0; // Start index
+
+        // Find the maximum length in the populated table
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (dp[i][j] == true) {
+                    if (j - i + 1 > ans) {
+                        ans = j - i + 1;
+                        x = i;
+                    }
+                }
+            }
+        }
+
+        return s.substr(x, ans);
     }
 };
