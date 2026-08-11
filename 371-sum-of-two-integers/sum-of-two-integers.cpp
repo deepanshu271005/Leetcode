9f77bc9ab6be25ce4b1d67270f9b561666a6d250
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-11 10:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (67.55%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -10,24 +10,24 @@ public:
             
             int current_bit = 0;
             
-            // Condition 1: Both bits are 1
+            // Condition 1 Both bits are 1
             if (bit_a == 1 && bit_b == 1) {
-                current_bit = carry; // If carry is 1, sum is 3 (11 in binary), so bit is 1. If 0, sum is 2 (10), so bit is 0.
-                carry = 1;           // Carry is always 1 when both bits are 1
+                current_bit = carry; 
+                carry = 1;
             } 
-            // Condition 2: Both bits are 0
+            // Condition 2 Both bits are 0
             else if (bit_a == 0 && bit_b == 0) {
                 current_bit = carry; 
-                carry = 0;           // Carry is always 0 when both bits are 0
+                carry = 0;          
             } 
-            // Condition 3: One bit is 1, the other is 0
+            // Condition 3 One bit is 1, the other is 0
             else {
                 if (carry == 1) {
                     current_bit = 0; 
-                    carry = 1;       // 1 + 1 (from carry) = 2 (10 in binary)
+                    carry = 1;       
                 } else {
                     current_bit = 1; 
-                    carry = 0;       // 1 + 0 (from carry) = 1 (01 in binary)
+                    carry = 0;        
                 }
             }
             
