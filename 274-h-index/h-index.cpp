b591d69fb6d5e47fe7f8488205408dc2e760c793
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-16 03:33:03 +0530

    Time: 3 ms (13.13%) | Memory: 12.2 MB (64.16%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         
-       sort(citations.begin(),citations.end());
+      // sort(citations.begin(),citations.end());
          int best=0;
          int n=citations.size();
          for(int curr=1;curr<=1000;curr++){
