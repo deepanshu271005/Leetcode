73a5d77094a4b46c13e4cc725938d94922a8c6ab
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-17 22:23:56 +0530

    Time: 1026 ms (9.68%) | Memory: 27.8 MB (21.68%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    int stoneGameV(vector<int>& a) {
-        int n = a.size();
-        int dp[n][n];
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
 
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=0; j<=(n-i); j++)
-            {
-                dp[j][i+j-1] = 0;
-                if(i != 1)
-                {
-                    int sum = 0, sum2 = 0;
-                    for(int k=j; k<(j+i); k++) sum += a[k];
-                    for(int k=j; k<(j+i-1); k++)
-                    {
-                        sum2 += a[k];
-                        sum -= a[k];
-                        if(sum2 > sum) dp[j][j+i-1] = max(dp[j][j+i-1], sum + dp[k+1][j+i-1]);
-                        else if(sum2 < sum) dp[j][j+i-1] = max(dp[j][j+i-1], sum2 + dp[j][k]);
-                        else
-                        {
-                             dp[j][j+i-1] = max(dp[j][j+i-1], sum + dp[j][k]);
-                             dp[j][j+i-1] = max(dp[j][j+i-1], sum + dp[k+1][j+i-1]);
-                        }
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        vector<int> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + stoneValue[i];
+        }
+
+        for (int len = 2; len <= n; ++len) {
+
+            for (int i = 0; i <= n - len; ++i) {
+
+                int j = i + len - 1;
+
+                for (int k = i; k < j; ++k) {
+
+                    int left_sum = prefix[k + 1] - prefix[i];
+                    int right_sum = prefix[j + 1] - prefix[k + 1];
+
+                    if (left_sum < right_sum) {
+
+                        dp[i][j] = max(dp[i][j], left_sum + dp[i][k]);
+                    } else if (left_sum > right_sum) {
+
+                        dp[i][j] = max(dp[i][j], right_sum + dp[k + 1][j]);
+                    } else {
+
+                        dp[i][j] = max({dp[i][j], left_sum + dp[i][k],
+                                        right_sum + dp[k + 1][j]});
                     }
                 }
             }
         }
 
-        return dp[0][n-1];
-
+        return dp[0][n - 1];
     }
 };
