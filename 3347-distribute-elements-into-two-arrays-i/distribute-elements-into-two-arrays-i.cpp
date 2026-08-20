82cb84e56679c24d397d6b4d2a9042924c97d94a
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-20 13:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (45.52%) - LeetSync

---
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int>nums1;
+        vector<int>nums2;
+        nums1.push_back(nums[0]);
+        nums2.push_back(nums[1]);
+        for(int i=2;i<nums.size();i++){
+            if(nums1.back()>nums2.back()){
+                nums1.push_back(nums[i]);
+            }
+            else nums2.push_back(nums[i]);
+        }
+        for(auto i:nums2){
+            nums1.push_back(i);
+        }
+        return nums1;
+
+    }
+};
