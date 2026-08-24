5efc20d236a7571aade93d29998b51dc1c10e8fa
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-24 19:31:18 +0530

    Time: 111 ms (5.15%) | Memory: 31.1 MB (25.85%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        priority_queue<long long,vector<long long >,greater<long long>>pq;
+        pq.push(1);
+        set<int>s;
+        while(s.size()<=n){
+            long long i=pq.top();
+            pq.pop();
+            if(s.find(i)!=s.end()){continue;}
+            pq.push(2*i);
+            pq.push(3*i);
+            pq.push(5*i);
+            s.insert(i);
+        }
+        auto it=--s.rbegin();
+        return *it;
+
+    }
+};
