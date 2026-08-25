3a4b238c11089f602c9a285d5c844f476ab97e2c
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-25 19:06:55 +0530

    Time: 42 ms (72.90%) | Memory: 13.2 MB (71.05%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int>square;
-        int num=1;
-        while(num*num<=n){
-            square.push_back(num*num);
-            num++;
-        }
+        // vector<int>square;
+        // int num=1;
+        // while(num*num<=n){
+        //     square.push_back(num*num);
+        //     num++;
+        // }
  
       vector<int>dp(n+1,INT_MAX);
       dp[1]=1;
       dp[0]=0;
       for(int i=2;i<=n;i++){
-        for(int j=0;j<square.size() && square[j]<=i;j++){
-            dp[i]=min(dp[i],1+dp[i-square[j]]);
+        for(int j=1;j*j<=i;j++){
+            dp[i]=min(dp[i],1+dp[i-j*j]);
         }
       }
     //    for(auto i:square)cout<<i<<" ";
