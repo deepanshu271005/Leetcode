58a892273deb15cc0daa5b25d47cad6fd781259a
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-27 00:30:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (55.27%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-
-    long long power(long long a, long long b)
-    {
-        long long ans = 1;
-        while(b)
-        {
-            if(b%2)
-            {
-                ans *= a;
-            }
-            a *= a;
-            b/=2;
-        }
-        return ans;
-    }
     int integerBreak(int n) {
+      
+      if(n==2)return 1;
+      if(n==3)return 2;
 
-        long long a = 1;
+      vector<int>dp(n+1);
+      dp[2]=1;
+      dp[3]=2;
+      for(int i=4;i<=n;i++){
+        int spf = (dp[i-1] == 2) ? 1 : ((dp[i-1] % 2 == 0) ? 2 : 3);
+        dp[i]=dp[i-1]+(dp[i-1]/spf);
+      }
+    // for(int i=2;i<=n;i++){
+    //     cout<<dp[i]<<" ";
+    // }
+     return dp[n];
 
-        for(int k=2; k<=n; k++)
-        {
-            long long b1 = n/k;
-            long long e1 = k - n%k;
-            long long ans = power(b1, e1);
-            long long ans2 = power(b1+1, k - e1);
-
-            a = max(ans*ans2, a);
-
-            // cout << k << "  " << ans << " " << ans2 << endl;
-        }
-
-        return a;
     }
 };
