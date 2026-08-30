3b0d20d5cf5585c820a0346a0af3441c0d4d2226
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-30 15:54:19 +0530

    Time: 95 ms (12.32%) | Memory: 94.7 MB (11.81%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& nums) {
-         
-        sort(nums.begin(), nums.end(), [](const vector<int>& a, const vector<int>& b){
-            if(a[0] == b[0]) return a[1] > b[1];
-            return a[0] < b[0];
-        });
-        //custom sort to sort the first element in the assendiign order and the seond element in the dexcendin order 
-        vector<int>LIS;
-        for(auto i:nums){
-            LIS.push_back(i[1]);
+
+        // sort(nums.begin(), nums.end(),
+        //      [](const vector<int>& a, const vector<int>& b) {
+        //          if (a[0] == b[0])
+        //              return a[1] > b[1];
+        //          return a[0] < b[0];
+        //      });
+
+        // custom sort to sort the first element in the assendiign order and the
+        // seond element in the dexcendin order
+
+
+    for(auto &i:nums){
+        i[1]=i[1]*(-1);
+    }
+    sort(nums.begin(),nums.end());
+        vector<int> LIS;
+        for (auto i : nums) {
+            LIS.push_back(-1*i[1]);
         }
-        vector<int>tail;
-        
-        for(auto i:LIS){
-            
-            auto it=lower_bound(tail.begin(),tail.end(),i);
-            if(it==tail.end())tail.push_back(i);
-            else *it=i;
-           
-        } 
-         
+        vector<int> tail;
 
-          return tail.size();
+        for (auto i : LIS) {
 
+            auto it = lower_bound(tail.begin(), tail.end(), i);
+            if (it == tail.end())
+                tail.push_back(i);
+            else
+                *it = i;
+        }
+
+        return tail.size();
     }
 };
