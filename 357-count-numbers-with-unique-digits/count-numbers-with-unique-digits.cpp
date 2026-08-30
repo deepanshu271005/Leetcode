1cc09d4cd95a8588f6c6a34fcc7b5dacef543248
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-30 21:17:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (44.89%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n==0)return 1;
+       vector<int>dp(n+1,0);
+       dp[0]=1;
+       dp[1]=9;
+       for(int i=2;i<=n;i++){
+             dp[i]=dp[i-1]*(11-i);
+       }
+   
+   int ans=0;
+   for(auto i:dp)ans+=i;
+   return ans;
+
+      
+       
+    }
+};
