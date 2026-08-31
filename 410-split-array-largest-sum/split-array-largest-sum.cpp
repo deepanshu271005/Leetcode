d84ee40e7dc13d4462da1045b68578b5d6f9822a
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-08-31 12:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (73.64%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,34 +1,83 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(k+1,INT_MAX));
-
-        //dp[i][k]->what is the minimised largest sum that i can get from 0 to i array if i split the array into k parts 
-       
-        vector<int>prefix(n+1);
-        prefix[0]=0;
-        for(int i=0;i<n;i++){
-            prefix[i+1]=prefix[i]+nums[i];
-        }
-        //prefix is one based index because my dp is also one based indexing 
-        //base case if k=1;
-
-        for(int i=1;i<=n;i++){
-            dp[i][1]=prefix[i];
-        }
-
-        //if  a state dp[i][k] is INT_MAX this means its impossible state that means that we cant cut k parts from it 
-
-        for(int i=2;i<=n;i++){
-            for(int j=2;j<=k;j++){
-               // if(j>i)break;
-                for(int cut=1;cut<=i;cut++){
-                    dp[i][j]= min(dp[i][j],max(dp[cut][j-1],prefix[i]-prefix[cut]));
-                }
+          
+    bool f(int mid,vector<int>&nums,int k){
+        int cnt=1;
+        int curr_sum=0;
+        for(auto i:nums){
+            curr_sum+=i;
+            if(curr_sum>mid){
+                cnt++;
+                curr_sum=i;
+                if(curr_sum>mid)return false;
             }
+            
         }
+        if(cnt<=k){
+            return true;
+        }
+        return false;
+    }
 
-       return dp[n][k];
+    int splitArray(vector<int>& nums, int k) {         
+         // so i will now use the binary seach to solve this 
+         int l=0;int r=1e9;
+         int ans;
+         while(l<=r){
+            int mid=l+(r-l)/2;
+            bool result=f(mid,nums,k);
+            if(result){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+         }
+         return ans;
+
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     int splitArray(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         vector<vector<int>>dp(n+1,vector<int>(k+1,INT_MAX));
+
+//         //dp[i][k]->what is the minimised largest sum that i can get from 0 to i array if i split the array into k parts 
+       
+//         vector<int>prefix(n+1);
+//         prefix[0]=0;
+//         for(int i=0;i<n;i++){
+//             prefix[i+1]=prefix[i]+nums[i];
+//         }
+//         //prefix is one based index because my dp is also one based indexing 
+//         //base case if k=1;
+
+//         for(int i=1;i<=n;i++){
+//             dp[i][1]=prefix[i];
+//         }
+
+//         //if  a state dp[i][k] is INT_MAX this means its impossible state that means that we cant cut k parts from it 
+
+//         for(int i=2;i<=n;i++){
+//             for(int j=2;j<=k;j++){
+//                  if(j>i)break;
+//                 for(int cut=1;cut<=i;cut++){
+//                     dp[i][j]= min(dp[i][j],max(dp[cut][j-1],prefix[i]-prefix[cut]));
+//                 }
+//             }
+//         }
+
+//        return dp[n][k];
+//     }
+// };
+
+
+
+ 
