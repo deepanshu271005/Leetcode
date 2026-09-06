c8d8f35663992fd6c2d8da1e81fb83fb43c94918
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-09-06 18:13:13 +0530

    Time: 23 ms (77.66%) | Memory: 27.1 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-
-    int f(string & s,string & t,int index, int j,vector<vector<int>>&dp ){
-        //base case
-        if(j==-1)return 1;
-        if(index<0)return 0;
-        
-      //  if(size<=0)return 0;
-    if(dp[index][j]!=-1)return dp[index][j];
-
-   //character lene wala case 
-        int op1=0;int op2=0;int op3=0;
-        if(s[index]==t[j]) { 
-            //take
-            op1=f(s,t,index-1,j-1,dp);
-            // not take 
-            op2=f(s,t,index-1,j,dp);}
-            else{
-                  op3=f(s,t,index-1,j,dp);
-            }
-        
-        
-        
-          
-        
-
-        //charater not pick wala case
-        
-
-       return  dp[index][j]=op1+op2+op3;
-        
-    }
-   
     int numDistinct(string s, string t) {
-         int index=s.size()-1;
-         int size=t.size();
-         vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+        int n=s.size();
+        int m=t.size(); 
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(m+1,0));
+        //dp[i][j]->tells the ways that we can make the string upto len i from the other string to the len j 
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(s[i]==t[j]){
+                    dp[i+1][j+1]+=dp[i][j];//this is the ways if we choose to include the current character and check the last i,j 
+                    dp[i+1][j+1]+=dp[i][j+1];//if we choose not to take the i+1 char then we check the i,j+1 gives us what 
+                }
+                else {
+                    dp[i+1][j+1]+=dp[i][j+1];//character mismatches is simply take the last i,j+1 ways
+                }
+            }
+        }
 
-     return f(s,t,index,t.size()-1,dp);
-
+        return dp[n][m];
+ 
     }
 };
