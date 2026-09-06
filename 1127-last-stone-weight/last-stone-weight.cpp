2d2dfdbaffcf15c76c4c871491e6c2a7cbaacbf3
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-09-06 18:48:42 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (50.70%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto i:stones){
+            pq.push(i);
+        }
+        
+        while(pq.size()>1){
+            int a =pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            if(a==b)continue;
+            pq.push(abs(b-a));
+        }
+        return (pq.empty())?0:pq.top();
+
+    }
+};
